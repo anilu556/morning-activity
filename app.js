eac6bc10561922e8325ff60def7a210c5d79fd52
anilu556 <anilu556@hotmail.com>
2018-12-20 13:15:38 -0600

    exercise done

---
+++ app.js
@@ -0,0 +1,115 @@
+//Refactor each function according to the directions given. The tests will still pass if you use Es5 syntax, but the spirit of this exercise is to use Es6 syntax and then compare the two functions to one another. Your refactored code should go in the functions with "your code goes here".
+
+// refactor this to use the es6 destructuring syntax (hint: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment)
+//x and y are index numbers, this function swaps the values at those indexes
+function swap(arr,x,y){
+  var temp = arr[x];
+  arr[x] = arr[y];
+  arr[y] = temp;
+}
+
+function swapEs6(arr,x,y){
+  //Your code here
+  [arr[x], arr[y]] = [arr[y], arr[x]]
+}
+
+
+//refactor this to use es6 for...of syntax
+function changeEachElem(array){
+  var result = [];
+  for (var i = 0; i < array.length; i++) {
+    result.push(array[i]);
+  }
+  return result;
+}
+
+function changeEachElemEs6(array){
+  //Your code here
+var result = []
+
+for (let value of array) {
+  result.push(value)
+}
+return result;
+}
+
+
+//refactor this to use es6 arrow function
+function useArrows(array){
+  var result = array.map(function(element){
+    return element * 2;
+  });
+
+  return result;
+}
+
+
+  //Your code here
+  useArrowsEs6 = array => array.map( element => element*2);
+
+
+//refactor this to use the backticks for string interpolation
+function printString(name,greeting){
+  return name + ' says "' + greeting +'"';
+}
+
+  //Your code here
+  printStringEs6 = (name, greeting) => `${name} says "${greeting}"` ;
+
+
+//refactor this to  be more concise by using new es6 syntax for 'computed property names'
+function makeObj(val1,val2){
+  var num = val1 + val2;
+  var myObj = {};
+  myObj[num] = 'Shayna';
+  return myObj;
+}
+
+/*function makeObjEs6(val1,val2){
+  //Your code here
+  myObj = {
+    [val1 + val2]: 'Shayna'
+  }
+  return myObj
+}*/
+
+  makeObjEs6 = (val1,val2) => ({[val1 + val2] : 'Shayna'});
+
+
+
+//refactor this to be more concise by using new es6 syntax (hint https://ariya.io/2013/02/es6-and-object-literal-property-value-shorthand)
+function ObjProperties(name, grade, favColor) {
+  return {
+    name: name,
+    grade: grade,
+    favColor: favColor
+  };
+}
+
+/*function ObjPropertiesEs6(name, grade, favColor) {
+  //Your code here
+  return {
+    name, grade, favColor
+  };
+}*/
+
+ObjPropertiesEs6 = (name,grade,favColor) => ({name, grade, favColor});
+
+
+
+/* DON’T TOUCH */
+
+module.exports = {
+  swap,
+  swapEs6,
+  changeEachElem,
+  changeEachElemEs6,
+  useArrows,
+  useArrowsEs6,
+  printString,
+  printStringEs6,
+  makeObj,
+  makeObjEs6,
+  ObjProperties,
+  ObjPropertiesEs6
+};
